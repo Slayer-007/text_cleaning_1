fea863a0583c9456e13eedc39cff65ddab849e11
Slayer-007 <saurabhyad007@gmail,com>
2021-07-30 18:34:53 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+#Basic text cleaning
+
+
+### This file is made to perform some basic text cleaning,like removing html tags,website addresses,mail address and removing some most common names
+
+This python package is prepared by Saurabh Yadav.
+
+
---
+++ setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+with open('readme.md','r') as file:
+	long_description=file.read()
+
+setuptools.setup(name='clean_saurabh',
+version='0.0.1',
+author_name='Saurabh Yadav',
+author_email='saurabhyad007@gmail.com',
+description='Basic Cleaning',
+long_description=long_description,
+long_description_content_type='text/markdown',
+packages=setuptools.find_packages,
+python_requires='>=3.6',
+classifiers=['Programming Language::Python::3',
+'License::OSI Approved ::MIT License',
+'Operating System ::OS Independent']) #this name should be globally unique
+
